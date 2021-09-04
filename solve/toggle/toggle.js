2fdafa0f98f4018924a2879b3450c4bd2caa0550
Bill Konchellah <williamoriginal2019@gmail.com>
2021-09-04 22:03:59 +0300

    solved toggle task

--- solve/toggle/toggle.js
+++ solve/toggle/toggle.js
@@ -2,7 +2,16 @@
 export const ToggleModule = () => {
 
     //Write solution to the Toggle task inside this ToggleModule function.
-    console.log('checking toggle...');
+    document.getElementById("switch").addEventListener("click", function() {
+        let innerText = document.getElementById("status").innerHTML;
+        if(innerText == 'OFF'){
+            document.getElementById("status").innerHTML = "ON";
+            document.getElementById("status").className = "on";
+        }else{
+            document.getElementById("status").innerHTML = "OFF";
+            document.getElementById("status").className = "off";
+        }
+      }); 
     
 }
 
